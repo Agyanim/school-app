ad671c14f9e0ab2647afd5526d65807b8ab968b8
Agyanim <71318355+Agyanim@users.noreply.github.com>
2024-02-15 23:35:26 +0000

    layout modified

--- src/util/jwt.ts
+++ src/util/jwt.ts
@@ -1,59 +1,64 @@
 import jwt from "jsonwebtoken";
-import { jwtVerify,SignJWT } from "jose";
+import { jwtVerify, SignJWT } from "jose";
 import { cookies } from "next/headers";
 
-const accessTokenSecretKey = process.env.JWT_SECRET ||'';
+const accessTokenSecretKey = process.env.JWT_SECRET || "";
 const refreshTokenSecretKey = process.env.JWT_REFRESH_SECRET || "";
 
-interface userJwtPayload{
-  jti:string
-  iat:number
+interface userJwtPayload {
+	jti: string;
+	iat: number;
 }
 export const registerAccessToken = (email: string) => {
-  const payload = {
-    email: email,
-  };
-  try {
-    const token = jwt.sign(payload, accessTokenSecretKey, { expiresIn: "1m" });
-    cookies().set({
-      name: "token",
-      value: token,
-      httpOnly: true,
-    });
-    return token;
-  } catch (error: any) {
-  }
+	const payload = {
+		email: email,
+	};
+	try {
+		const token = jwt.sign(payload, accessTokenSecretKey, { expiresIn: "1m" });
+		cookies().set({
+			name: "token",
+			value: token,
+			httpOnly: true,
+		});
+		return token;
+	} catch (error: any) {}
 };
 export const registerRefreshToken = (email: string) => {
-  const payload = {
-    email: email,
-  };
-  try {
-    const token = jwt.sign(payload, refreshTokenSecretKey, { expiresIn: "7days" });
-    return token;
-  } catch (error: any) {
-    throw new Error(error.message);
-  }
+	const payload = {
+		email: email,
+	};
+	try {
+		const token = jwt.sign(payload, refreshTokenSecretKey, {
+			expiresIn: "7days",
+		});
+		return token;
+	} catch (error: any) {
+		throw new Error(error.message);
+	}
 };
-export const getJwtSecret=()=>{
-  const accessTokenSecret = accessTokenSecretKey;
-  return accessTokenSecret
-}
-export const verifyAccessToken = async ( accessToken: string) => {
-  try {
-   const verifiedToken= await jwtVerify(accessToken, new TextEncoder().encode(getJwtSecret()));   
-   return verifiedToken.payload as userJwtPayload
-  } catch (error: any) {
-    throw new Error("error occured")
-  }
+export const getJwtSecret = () => {
+	const accessTokenSecret = accessTokenSecretKey;
+	if (!accessTokenSecret || accessTokenSecret != "") {
+		return accessTokenSecret;
+	}
 };
-
+export const verifyAccessToken = async (accessToken: string) => {
+	try {
+		const verifiedToken = await jwtVerify(
+			accessToken,
+			new TextEncoder().encode(getJwtSecret())
+		);
+		return verifiedToken.payload as userJwtPayload;
+	} catch (error: any) {
+		throw new Error("error occured");
+	}
+};
 
 export const verifyRefreshToken = (accessToken: string) => {
-  try {
-    const payload = jwt.verify(accessToken, refreshTokenSecretKey);
-    return payload;
-  } catch (error: any) {
-    throw new Error(error.message);
-  }
+	try {
+		const payload = jwt.verify(accessToken, refreshTokenSecretKey);
+		return payload;
+	} catch (error: any) {
+		throw new Error(error.message);
+	}
 };
