395482643331ada94b4f714fc7798a45a32e1f07
Agyanim <71318355+Agyanim@users.noreply.github.com>
2024-02-26 20:59:21 +0000

    rightbar

--- src/components/client/dashboard/index.ts
+++ src/components/client/dashboard/index.ts
@@ -4,5 +4,5 @@ import SearchBar from "./navbar/SearchBar"
 import Card from "./cards/Card"
 import Transaction from "../transaction/Transaction"
 import Chart from "./chart/Chart"
-import RightBar from "../rightbar/Rightbar"
+import RightBar from "../rightbar/RightBar"
 export {Sidebar,Navbar ,SearchBar, Card,Transaction,Chart,RightBar}
