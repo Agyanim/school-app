2a98403d5e0526de60c3a5fbb327ee2719369700
agyanim <agyanimconsultancy@gmail.com>
2024-02-29 16:36:32 +0000

    profile image added to folder

--- src/app/api/dashboard/upload/route.ts
+++ src/app/api/dashboard/upload/route.ts
@@ -1,14 +1,36 @@
 import { NextRequest, NextResponse } from "next/server";
+import path from "path";
+import fs from 'fs'
+import { writeFile } from "fs/promises";
 
 export const POST = async (request: NextRequest) => {
   const data = await request.formData();
   const file: File | null = data.get("file") as unknown as File;
-  if (!file) {
-    return NextResponse.json({ success: false });
+  try {
+    if (!file) {
+      return NextResponse.json({ success: false });
+    }
+    
+    const bytes = await file.arrayBuffer();
+    const buffer = Buffer.from(bytes);
+    // console.log(buffer);
+    const myPath=path.join(process.cwd(),`public/upload/${file.name}`)
+     fs.writeFileSync(myPath, buffer)
+    
+    return NextResponse.json({ success: true });
+    
+  } catch (error:any) {
+    console.log(error.message);
+    
   }
-  const bytes = await file.arrayBuffer();
-  const buffer = Buffer.from(bytes);
-  console.log(buffer);
-
-  return NextResponse.json({ success: true });
 };
+
+export const GET=async()=>{
+  console.log('get');
+  
+  const myPath=path.join(process.cwd(),"/public/upload/working-hours.jpg")
+  const file= fs.readFileSync(myPath)
+  console.log(file);
+  
+  return NextResponse.json({success:true,data:file})
+}
