0e363f1e768854061f18f13755e6d1886f23b972
agyanim <agyanimconsultancy@gmail.com>
2024-03-01 16:22:06 +0000

    profile image added

--- src/app/(dashboard)/dashboard/profile/page.tsx
+++ src/app/(dashboard)/dashboard/profile/page.tsx
@@ -1,10 +1,29 @@
 "use client";
 import { axiosInstance } from "@/axios-handlers";
 import axios from "axios";
+import { readFileSync } from "fs";
+import { readFile } from "fs/promises";
+import { url } from "inspector";
+import { GetServerSideProps } from "next";
+import Image from "next/image";
+import path from "path";
 import React, { ChangeEvent, FormEvent, useState } from "react";
+import fs from "fs";
+
+interface Props {
+  dirs: string[];
+}
+
 const ProfilePage = () => {
   const [file, setFile] = useState<File>();
+  const [profileIMage, setProfileImage] = useState("");
+  const [selectedIMage, setSelectedImage] = useState("");
 
+  const onChangeHanlder = (e: any) => {
+    const file = e.target.files?.[0];
+    setSelectedImage(URL.createObjectURL(file));
+    setFile(file);
+  };
   const uploadProfileImage = async (e: FormEvent) => {
     e.preventDefault();
     if (!file) return;
@@ -15,8 +34,13 @@ const ProfilePage = () => {
         method: "post",
         body: formData,
       });
-      if (!res) return;
-      console.log(res);
+      // if (!res) return;
+      // const response = await fetch("/api/dashboard/upload", {
+      //   method: "get",
+      // });
+      // if (!response) return;
+      const data = await res.json();
+      setProfileImage(data.imageUrl);
     } catch (error: any) {
       console.log(error.message);
     }
@@ -28,13 +52,23 @@ const ProfilePage = () => {
           type="file"
           name="profileImage"
           id="profileImage"
-          onChange={(e) => {
-            setFile(e.target.files?.[0]);
-          }}
+          onChange={onChangeHanlder}
         />
         <br />
         <button type="submit">Send</button>
       </form>
+      {selectedIMage && (
+        <div>
+          <img src={selectedIMage} width={100} height={100} alt="profile" />
+        </div>
+      )}
+      {profileIMage && (
+        <div>
+          <img src={profileIMage} alt="text" />
+        </div>
+      )}
+
+      <div></div>
     </div>
   );
 };
--- src/app/api/dashboard/upload/route.ts
+++ src/app/api/dashboard/upload/route.ts
@@ -1,9 +1,9 @@
 import { NextRequest, NextResponse } from "next/server";
 import path from "path";
 import fs from 'fs'
-import { writeFile } from "fs/promises";
-
 export const POST = async (request: NextRequest) => {
+  // const form =formidable()
+  // form.parse(request,(err:any,fields: formidable.Fields<string>, files: formidable.Files<string>)
   const data = await request.formData();
   const file: File | null = data.get("file") as unknown as File;
   try {
@@ -16,8 +16,9 @@ export const POST = async (request: NextRequest) => {
     // console.log(buffer);
     const myPath=path.join(process.cwd(),`public/upload/${file.name}`)
      fs.writeFileSync(myPath, buffer)
+     const imageUrl=`/upload/${file.name}`
     
-    return NextResponse.json({ success: true });
+    return NextResponse.json({ success: true ,imageUrl});
     
   } catch (error:any) {
     console.log(error.message);
@@ -25,12 +26,8 @@ export const POST = async (request: NextRequest) => {
   }
 };
 
-export const GET=async()=>{
-  console.log('get');
-  
+export const GET=async()=>{  
   const myPath=path.join(process.cwd(),"/public/upload/working-hours.jpg")
   const file= fs.readFileSync(myPath)
-  console.log(file);
-  
   return NextResponse.json({success:true,data:file})
 }
