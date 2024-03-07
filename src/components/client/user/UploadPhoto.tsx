5ba5c83befbc40b2bd3930dc092242b4a9e61df2
agyanim <agyanimconsultancy@gmail.com>
2024-03-07 16:06:33 +0000

    upload photo form

--- src/components/client/user/UploadPhoto.tsx
+++ src/components/client/user/UploadPhoto.tsx
@@ -1,16 +1,18 @@
 "use client";
 import { UseUploadProfileImageContext } from "@/context/UploadImageContext";
-import React, {FormEvent } from "react";
+import React, { FormEvent } from "react";
 
 const UploadPhoto = () => {
-  const { setFile, file, setProfileImage, setSelectedImage,selectedImage } =
+  const { setFile, file, setProfileImage, setSelectedImage, selectedImage } =
     UseUploadProfileImageContext();
 
   const onChangeHanlder = (e: any) => {
     const newFile = e.target.files?.[0];
-    const imageUrl = URL.createObjectURL(newFile);
-    setSelectedImage({selectedImage:imageUrl});
-    setFile({file:newFile});
+    if (newFile) {
+      const imageUrl = URL.createObjectURL(newFile);
+      setSelectedImage({ selectedImage: imageUrl });
+      setFile({ file: newFile });
+    }
   };
   const uploadProfileImage = async (e: FormEvent) => {
     e.preventDefault();
@@ -23,7 +25,7 @@ const UploadPhoto = () => {
         body: formData,
       });
       const data = await res.json();
-      setProfileImage({profileImage:data.imageUrl});
+      setProfileImage({ profileImage: data.imageUrl });
     } catch (error: any) {
       console.log(error.message);
     }
@@ -31,9 +33,25 @@ const UploadPhoto = () => {
 
   return (
     <div className="mt-10">
-      <form className="w-[9rem]  flex flex-col gap-4" onSubmit={uploadProfileImage}>
-        <label className="flex justify-center items-center bg-gray-400/20  h-[10rem] rounded hover:cursor-pointer" htmlFor="profileImage">
-          {selectedImage? <img  className="h-full w-full" src={selectedImage} alt="selected image" />:<p className="h-[6rem] w-[4rem] flex justify-center items-center text-orange-700 font-bold " >Select Image</p>}
+      <form
+        className="w-[9rem]  flex flex-col gap-4"
+        onSubmit={uploadProfileImage}
+      >
+        <label
+          className="flex justify-center items-center bg-gray-400/20  h-[10rem] rounded hover:cursor-pointer"
+          htmlFor="profileImage"
+        >
+          {selectedImage ? (
+            <img
+              className="h-full w-full"
+              src={selectedImage}
+              alt="selected image"
+            />
+          ) : (
+            <p className="h-[6rem] w-[4rem] flex justify-center items-center text-orange-700 font-bold ">
+              Select Image
+            </p>
+          )}
           <input
             type="file"
             name="profileImage"
@@ -42,7 +60,12 @@ const UploadPhoto = () => {
             onChange={onChangeHanlder}
           />
         </label>
-        <button className="bg-gradient-to-r from-orange-600 to-pink-600 border-none hover:bg-gradient-to-l rounded py-2 px-3 text-white font-bold" type="submit">Upload Image</button>
+        <button
+          className="bg-gradient-to-r from-orange-600 to-pink-600 border-none hover:bg-gradient-to-l rounded py-2 px-3 text-white font-bold"
+          type="submit"
+        >
+          Upload Image
+        </button>
       </form>
     </div>
   );
