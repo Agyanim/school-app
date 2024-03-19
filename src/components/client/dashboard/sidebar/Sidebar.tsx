258b6ce6b687915cf5df4c9e7af7271c38a9f955
agyanim <agyanimconsultancy@gmail.com>
2024-03-19 16:14:41 +0000

    local storage added

--- src/components/client/dashboard/sidebar/Sidebar.tsx
+++ src/components/client/dashboard/sidebar/Sidebar.tsx
@@ -7,36 +7,55 @@ import { AiOutlineLogout } from "react-icons/ai";
 import MenuList from "./MenuList";
 import { UseUploadProfileImageContext } from "@/context/UploadImageContext";
 import { getCurrentUserByIdQuery } from "@/query-handlers/userQueries";
+import { url } from "inspector";
 
 const Sidebar = () => {
-  const {currentUserId } = UseUploadProfileImageContext();
-  const { data } = getCurrentUserByIdQuery(currentUserId);
-  // console.log(currentUserId);
-  
+  const { currentUserId } = UseUploadProfileImageContext();
+  if (currentUserId) {
+    localStorage.setItem("currentUserId", JSON.stringify(currentUserId));
+  }
+  let userId: string = localStorage.getItem("currentUserId") || currentUserId;
+  if (userId) {
+    userId = JSON.parse(userId);
+  }
+  const { data } = getCurrentUserByIdQuery(userId);
+
+  // console.log(userId);
 
   return (
     <section className="bg-gradient-to-r from-orange-800 to-pink-800 w-[20%] shadow-slate-950 sticky shadow-inner">
       <div className="flex flex-col items-start mt-10 ml-5 gap-10">
-        <div className="flex gap-2 items-center">
+        <div className="flex gap-2 items-center w-full ">
           {data?.user?.profile?.imageUrl ? (
-            <Image
-              className="rounded-[50%]"
-              src={data?.user?.profile?.imageUrl}
-              alt="profileImage"
-              width={50}
-              height={50}
-            />
+            <div style={{
+            }} className="w-[6rem]  h-[6rem] p-2  overflow-hidden flex items-center justify-center">
+              
+              <Image
+                className="rounded-[50%] "
+                src={data?.user?.profile?.imageUrl}
+                alt="profileImage"
+                width={50}
+                height={50}
+                sizes="cover"
+              />
+            </div>
           ) : (
-            <Image
-              className="rounded-[50%]"
-              src={ProfileImage}
-              alt="profileImage"
-              width={50}
-              height={50}
-            />
+            <div className="">
+              <Image
+                className="rounded-[50%]"
+                src={ProfileImage}
+                alt="profileImage"
+                width={50}
+                height={50}
+              />
+            </div>
           )}
           <div className="text-white/80 text-sm">
-            <h1>{data?.user?.profile?.userName? data?.user.profile.userName :'UserName'}</h1>
+            <h1>
+              {data?.user?.profile?.userName
+                ? data?.user.profile.userName
+                : "UserName"}
+            </h1>
             <p className="italic">Profession</p>
           </div>
         </div>
